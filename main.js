9f030617114b870b6852ac84423acd0008e87550
Lois2323-23 <loiscandrasusanto@gmail.com>
2025-08-25 17:32:48 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Saya seorang siswa", "SMKN 2 Tanjung Selor", "Dengan Jurusan RPL"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
